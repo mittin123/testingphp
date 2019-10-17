3ca4ce796699c115676d6367ec8625c84252a762
mittin123 <kyoshirox1104@gmail.com>
2019-10-17 10:05:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,18 @@
 <head>
 	<title>PHP Test</title>
 </head>
+<script type="text/javascript">
+	
+</script>
+<style type="text/css" media="screen">
+	
+</style>
 <body>
-<?php echo '<font color="red">Hello World</font>'; ?>
+	<div class="w3-red">
+		<?php 
+			echo 'Hello World'; 
+		?>
+	</div>
+
 </body>
 </html>
